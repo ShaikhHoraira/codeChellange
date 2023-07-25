2a5bf0815dbe356a71d2437c5154b410f36a0438
Shaikh <shaikh.abu.horaira@gmail.com>
2023-07-26 02:32:33 +1000

    some changes into directory and added unit testing not tested yet, slight improvement

--- test/handler/saveHandler.spec.ts
+++ test/handler/saveHandler.spec.ts
@@ -8,13 +8,12 @@ const ddbMock = mockClient(DynamoDBDocumentClient);
 AWS.config.update({ region: 'local' });
 
 const saveCustomerAddress = new SaveCustomerAddress('')
-describe('Manage PushMessage for Authenticated User', () => {
+describe('Tu Db input case', () => {
     beforeEach(async () => {
       jest.resetModules(); // Most important - it clears the cache
       AWSMock.setSDKInstance(AWS);
       AWSMock.mock('DynamoDB.DocumentClient', 'put', 'success');
       ddbMock.reset();
-      const dynamoDBMock = mockClient(DynamoDBDocumentClient);
     });
   
     afterAll(() => {
