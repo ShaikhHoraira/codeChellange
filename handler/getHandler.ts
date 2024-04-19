d7bf4980dc1d68ff6c3bad847b56b749c9ac81d6
Shaikh <shaikh.abu.horaira@gmail.com>
2024-04-20 00:14:41 +1000

    test commit to check cicd aws deploy

--- handler/getHandler.ts
+++ handler/getHandler.ts
@@ -7,7 +7,7 @@ export const handler: Handler = async (event : any) => {
     if (!event.queryStringParameters.userId || !event.queryStringParameters ) {
       return {
         statusCode: 400,
-        body: "Missing userId, Please provide userId",
+        body: "Missing userId, Please provide userId, and this is the test",
       };
     };
     const manageDevice = new GetCustomerAddress(event.queryStringParameters.userId,event.queryStringParameters.suburb, event.queryStringParameters.postcode);
