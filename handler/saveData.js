15e64c71b0cc96112077fb842561d85038e722ed
Shaikh <shaikh.abu.horaira@gmail.com>
2024-04-24 01:59:51 +1000

    only for typing

--- handler/saveData.js
+++ handler/saveData.js
@@ -2,15 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SaveCustomerAddress = void 0;
 const AWS = require("aws-sdk");
-
-
 const tableName = process.env.TABLE_NAME;
-
 const documentClient = new AWS.DynamoDB.DocumentClient({
     region: process.env.region,
 });
-
-
 class SaveCustomerAddress {
     constructor(payLoad) {
         this.payload = payLoad;
