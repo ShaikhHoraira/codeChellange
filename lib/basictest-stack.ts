8fed1e1202691b9d6f3f93f4f9b78e24d04f904b
Shaikh <shaikh.abu.horaira@gmail.com>
2024-04-23 23:57:18 +1000

    testing api

--- lib/basictest-stack.ts
+++ lib/basictest-stack.ts
@@ -10,60 +10,60 @@ export class BasictestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // const saveAddress = new Table(this, "Address", {
-    //   partitionKey: { name: "UserId", type: AttributeType.STRING },
-    //   tableName: "Tu_Test_TableName",
-    // });
-    // saveAddress.addGlobalSecondaryIndex({
-    //   indexName: 'UserIdIndex',
-    //   partitionKey: { name: 'UserId', type: AttributeType.STRING },
-    // });
+    const saveAddress = new Table(this, "Address", {
+      partitionKey: { name: "UserId", type: AttributeType.STRING },
+      tableName: "Tu_Test_TableName",
+    });
+    saveAddress.addGlobalSecondaryIndex({
+      indexName: 'UserIdIndex',
+      partitionKey: { name: 'UserId', type: AttributeType.STRING },
+    });
 
-    // const getUserdataLambda = new Function(this, "GetCustomerAddressLambdaHandler", {
-    //   runtime: Runtime.NODEJS_14_X,
-    //   code: Code.fromAsset('handler'),
-    //   handler: 'getHandler.handler',
-    //   environment: {
-    //     TABLE_NAME: saveAddress.tableName,
-    //   },
-    // });
+    const getUserdataLambda = new Function(this, "GetCustomerAddressLambdaHandler", {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset('handler'),
+      handler: 'getHandler.handler',
+      environment: {
+        TABLE_NAME: saveAddress.tableName,
+      },
+    });
 
-    //  const saveUserdataLambda = new Function(this, "PutCustomerAddressLambdaHandler", {
-    //   runtime: Runtime.NODEJS_14_X,
-    //   code: Code.fromAsset("handler"),
-    //   handler: "saveHandler.handler",
-    //   environment: {
-    //     TABLE_NAME: saveAddress.tableName,
-    //   },
-    // });
+     const saveUserdataLambda = new Function(this, "PutCustomerAddressLambdaHandler", {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset("handler"),
+      handler: "saveHandler.handler",
+      environment: {
+        TABLE_NAME: saveAddress.tableName,
+      },
+    });
     
-    // getUserdataLambda.role?.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonDynamoDBFullAccess'));
-    // saveAddress.grantWriteData(saveUserdataLambda);
+    getUserdataLambda.role?.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonDynamoDBFullAccess'));
+    saveAddress.grantWriteData(saveUserdataLambda);
 
-    // const api = new RestApi(this, "Tu_testApi", {
-    //   defaultMethodOptions: {
-    //     apiKeyRequired: true,
-    //   },
-    // });
-    // const userAddressApi = api.root.resourceForPath('userAddress');
+    const api = new RestApi(this, "Tu_testApi", {
+      defaultMethodOptions: {
+        apiKeyRequired: true,
+      },
+    });
+    const userAddressApi = api.root.resourceForPath('userAddress');
     
-    // userAddressApi.addMethod('GET', new LambdaIntegration(getUserdataLambda));
-    // userAddressApi.addMethod('POST', new LambdaIntegration(saveUserdataLambda));
+    userAddressApi.addMethod('GET', new LambdaIntegration(getUserdataLambda));
+    userAddressApi.addMethod('POST', new LambdaIntegration(saveUserdataLambda));
     
-    // const apiKey = api.addApiKey('ApiKey',{
-    //   apiKeyName: 'tuApiKey',
-    //   value: 'thisIsJustSampleAPi123' // we can get the apis using aws secret and get the key to fetch here 
-    // });
-    // const plan = api.addUsagePlan('Tu_api-usage-plan', { // we can use rate limit and other usage plans 
-    //   name: `api-usage-plan`,
-    //   apiStages: [{ stage: api.deploymentStage }],
-    // });
+    const apiKey = api.addApiKey('ApiKey',{
+      apiKeyName: 'tuApiKey',
+      value: 'thisIsJustSampleAPi123' // we can get the apis using aws secret and get the key to fetch here 
+    });
+    const plan = api.addUsagePlan('Tu_api-usage-plan', { // we can use rate limit and other usage plans 
+      name: `api-usage-plan`,
+      apiStages: [{ stage: api.deploymentStage }],
+    });
   
-    // plan.addApiKey(apiKey);
+    plan.addApiKey(apiKey);
   
-    // new CfnOutput(this, "API URL", {
-    //   value: api.url ?? "Something went wrong"
-    // });
+    new CfnOutput(this, "API URL", {
+      value: api.url ?? "Something went wrong"
+    });
 
   };
 }
