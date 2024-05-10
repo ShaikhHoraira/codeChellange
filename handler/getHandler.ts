465ab1e6fe014573a2fe4e9b45b7416831cb21c7
Shaikh <shaikh.abu.horaira@gmail.com>
2024-05-11 03:10:15 +1000

    changing node version to 16

--- handler/getHandler.ts
+++ handler/getHandler.ts
@@ -2,19 +2,20 @@ import { Handler } from "aws-lambda";
 import { GetCustomerAddress } from './getData';
 
 export const handler: Handler = async (event: any) => {
+  
   console.log("received UserId:", event)
   try {
     if (!event.queryStringParameters.userId || !event.queryStringParameters) {
-      return {
-        statusCode: 400,
-        body: "Missing userId, Please provide userId",
+      const response = {
+        statusCode: 204,
         headers: {
-          'Access-Control-Allow-Origin': '*', // or specific origin(s)
-          'Access-Control-Allow-Methods': 'OPTIONS, GET', // Include OPTIONS for preflight requests
-          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-          'Vary': 'Origin',
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "https://www.example.com",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
         },
-      };
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
     };
 
     const manageDevice = new GetCustomerAddress(event.queryStringParameters.userId, event.queryStringParameters.suburb, event.queryStringParameters.postcode);
