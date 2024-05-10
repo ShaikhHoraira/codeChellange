8ccd90d7cfd52cebf977b3f4572f3730098de004
Shaikh <shaikh.abu.horaira@gmail.com>
2024-05-11 04:31:40 +1000

    removed secret atm to look into it tomorrow

--- lib/basictest-stack.ts
+++ lib/basictest-stack.ts
@@ -1,12 +1,12 @@
 import { Construct } from 'constructs';
 import { Stack} from 'aws-cdk-lib';
-//import { RestApiConstruct } from '../construct/rest-api-construct';
+import { RestApiConstruct } from '../construct/rest-api-construct';
 
 export class BasictestStack extends Stack {
   constructor(scope: Construct, id: string, props?: any) {
     super(scope, id, props);
     
-    //const restApiConstruct = new RestApiConstruct(this, 'rest-api-construct', this);
-    console.log('restApiConstruct');
+    const restApiConstruct = new RestApiConstruct(this, 'rest-api-construct', this);
+    console.log(restApiConstruct);
   }
 }
