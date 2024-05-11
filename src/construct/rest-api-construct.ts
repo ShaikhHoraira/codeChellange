cd1df63f0e241a5b1b81ec9b0c4d09270d69f9fc
Shaikh <shaikh.abu.horaira@gmail.com>
2024-05-11 21:13:58 +1000

    fixed lambda handler path

--- src/construct/rest-api-construct.ts
+++ src/construct/rest-api-construct.ts
@@ -32,7 +32,7 @@ export class RestApiConstruct extends Construct {
 
     const getUserdataLambda = new Function(stack, "GetCustomerAddressLambdaHandler", {
       runtime: Runtime.NODEJS_20_X,
-      code: Code.fromAsset('handler'), 
+      code: Code.fromAsset('../handler'), 
       handler: 'getHandler.handler',
       environment: {
         TABLE_NAME: saveAddress.tableName,
@@ -47,7 +47,7 @@ export class RestApiConstruct extends Construct {
 
     const saveUserdataLambda = new Function(stack, "PutCustomerAddressLambdaHandler", {
       runtime: Runtime.NODEJS_20_X,
-      code: Code.fromAsset("handler"),
+      code: Code.fromAsset("../handler"),
       handler: 'saveHandler.handler',
       environment: {
         TABLE_NAME: saveAddress.tableName,
