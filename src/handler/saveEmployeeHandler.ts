07beef8fa02d366fa77c98c5bb19d3270f058104
Shaikh <shaikh.abu.horaira@gmail.com>
2024-05-14 19:00:52 +1000

    operation cost employee db test

--- src/handler/saveEmployeeHandler.ts
+++ src/handler/saveEmployeeHandler.ts
@@ -1,23 +1,23 @@
 import { Handler } from "aws-lambda";
-// import { SaveCustomerAddress } from '../modules/saveData';
+import { SaveCustomerAddress } from '../modules/saveData';
 
 export const handler: Handler = async (event: any) => {
   try { 
-    // if (event.httpMethod === "POST"){
-    //   const manageDevice = new SaveCustomerAddress(event);
-    //   await manageDevice.saveData();
-    // }
-    // // Construct the response
-    // const response = {
-    //   statusCode: 200,
-    //   body: JSON.stringify('Success')
-    // };
-    // return response;
-    console.log("lambda called", event.path)
-    return {
-        statusCode: 200,
-        body: event
+    if (event.httpMethod === "POST"){
+      const manageDevice = new SaveCustomerAddress(event);
+      await manageDevice.saveData();
     }
+    // Construct the response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify('Success')
+    };
+    return response;
+    // console.log("lambda called", event.path)
+    // return {
+    //     statusCode: 200,
+    //     body: event
+    // }
   } catch (e) {
     // Construct the error response
     const errorResponse = {
