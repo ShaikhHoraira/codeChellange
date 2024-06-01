f0de3cfb8f94cde9238078ba1f5a2b1a8f8a28ca
Shaikh <shaikh.abu.horaira@gmail.com>
2024-06-02 02:28:55 +1000

    secret retrivig from a module file

--- src/basictest-stack.ts
+++ src/basictest-stack.ts
@@ -1,17 +1,17 @@
 import { Construct } from 'constructs';
 import { Stack} from 'aws-cdk-lib';
 import { RestApiConstruct } from './construct/rest-api-construct';
-// import { OperatingcostConstruct } from './construct/rest-api-operatingcost';
+import { OperatingcostConstruct } from './construct/rest-api-operatingcost';
 import { ProductionCostConstruct } from './construct/rest-api-productioncost';
-// import { InfrastructureCostConstruct } from './construct/rest-api-infrastructurecost'
+import { InfrastructureCostConstruct } from './construct/rest-api-infrastructurecost'
 
 export class BasictestStack extends Stack {
   constructor(scope: Construct, id: string, props?: any) {
     super(scope, id, props);
     
     new RestApiConstruct(this, 'rest-api-construct', this);
-    // new OperatingcostConstruct(this, 'rest-api-operationcost', this);
+    new OperatingcostConstruct(this, 'rest-api-operationcost', this);
     new ProductionCostConstruct(this, 'rest-api-productioncost', this);
-    // new InfrastructureCostConstruct(this, 'rest-api-infrastructureCost', this);
+    new InfrastructureCostConstruct(this, 'rest-api-infrastructureCost', this);
   }
 }
