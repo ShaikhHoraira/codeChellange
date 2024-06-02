f169462036aa255ab5ebf93e31296792bc5ef57b
Shaikh <shaikh.abu.horaira@gmail.com>
2024-06-02 12:04:58 +1000

    git merge branch deployment

--- src/basictest-stack.ts
+++ src/basictest-stack.ts
@@ -2,8 +2,8 @@ import { Construct } from 'constructs';
 import { Stack} from 'aws-cdk-lib';
 import { RestApiConstruct } from './construct/rest-api-construct';
 import { OperatingcostConstruct } from './construct/rest-api-operatingcost';
-import { ProductionCostConstruct } from './construct/rest-api-productioncost';
-import { InfrastructureCostConstruct } from './construct/rest-api-infrastructurecost'
+// import { ProductionCostConstruct } from './construct/rest-api-productioncost';
+// import { InfrastructureCostConstruct } from './construct/rest-api-infrastructurecost'
 
 export class BasictestStack extends Stack {
   constructor(scope: Construct, id: string, props?: any) {
@@ -11,7 +11,7 @@ export class BasictestStack extends Stack {
     
     new RestApiConstruct(this, 'rest-api-construct', this);
     new OperatingcostConstruct(this, 'rest-api-operationcost', this);
-    new ProductionCostConstruct(this, 'rest-api-productioncost', this);
-    new InfrastructureCostConstruct(this, 'rest-api-infrastructureCost', this);
+    // new ProductionCostConstruct(this, 'rest-api-productioncost', this);
+    // new InfrastructureCostConstruct(this, 'rest-api-infrastructureCost', this);
   }
 }
